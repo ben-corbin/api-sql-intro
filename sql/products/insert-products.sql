7314ada419fcf026b76197ba9cdeeee117b5931b
Ben Corbin <ben@corbin.digital>
2022-03-24 17:47:43 +0000

    Core complete

--- sql/products/insert-products.sql
+++ sql/products/insert-products.sql
@@ -1,4 +1,4 @@
-insert into products(name, price, discount) VALUES ('apple', 135, false);
-insert into products(name, price, discount) VALUES ('orange', 55, true);
-insert into products(name, price, discount) VALUES ('kiwi', 165, true);
-insert into products(name, price, discount) VALUES ('banana', 035, false);
+INSERT into products(name, price, discount) VALUES ('apple', 135, false);
+INSERT into products(name, price, discount) VALUES ('orange', 55, true);
+INSERT into products(name, price, discount) VALUES ('kiwi', 165, true);
+INSERT into products(name, price, discount) VALUES ('banana', 035, false);
